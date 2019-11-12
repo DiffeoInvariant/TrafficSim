4f95fcc8b0ee1dd2d810baba9b7eb91ba0627deb
DiffeoInvariant <zane.jakobs@colorado.edu>
2019-11-12 14:38:02 -0700

    more boilerplate

---
+++ TrafficSim/include/highway.h
@@ -0,0 +1,199 @@
+#ifndef TS_HIGHWAY_H
+#define TS_HIGHWAY_H
+
+#include <petsc.h>
+#include <mpi.h>
+
+
+#define TS_COUNTY_NAME_LEN 3
+#define TS_MAX_CITY_NAME_LEN 50
+#define TS_MAX_ROAD_NAME_LEN 100
+
+
+typedef enum {
+	      TS_NS,
+	      TS_EW,
+	      TS_UNKNOWN_DIRECTION
+} TSRoadDirection;
+
+
+typedef enum {
+	      TS_INTERSTATE,
+	      TS_HIGHWAY,
+	      TS_SURFACE_STREET
+} TSRoadType;
+
+
+struct _ts_HighwayEntryCtx{
+  PetscReal                 postmile;
+  TSArrivalDistributionType arrival_dist;
+  PetscInt                  num_arrival_params;
+  PetscReal*                arrival_params;
+} PETSC_ATTRIBUTEALIGNED(sizeof(PetscScalar));
+
+typedef struct _ts_HighwayEntryCtx TSHighwayEntryCtx;
+
+extern PetscErrorCode HighwayEntryCreate(TSHighwayEntryCtx* entry, const PetscReal postmile,
+				         const TSArrivalDistributionType arrival_dist_t,
+				         const PetscInt* n_params, const PetscReal* params);
+
+extern PetscErrorCode StaticPoissonHighwayEntryCreate(TSHighwayEntryCtx*, const PetscReal, const PetscReal*);
+
+
+typedef enum {
+	      TS_STATIC_EXIT,/* time-independent exit probability for each passing car */
+	      TS_DYNAMIC_EXIT /* time-dependent exit probability for each passing car */
+} TSExitType;
+
+/* struct to hold params for multi-parameter distribution; TODO: maybe put this in the .c file? */
+struct _ts_multiparams{
+  PetscInt   n;
+  PetscReal* params;
+} PETSC_ATTRIBUTEALIGNED(sizeof(PetscScalar));
+
+typedef union {
+  PetscReal p;
+  struct _ts_multiparams params;
+} TSExitParams;
+  
+struct _ts_HighwayExitCtx{
+  PetscReal    postmile;
+  TSExitType   type;
+  TSExitParams prob_params;
+}PETSC_ATTRIBUTEALIGNED(sizeof(PetscScalar));
+
+typedef struct _ts_HighwayExitCtx TSHighwayExitCtx;
+
+extern PetscErrorCode HighwayExitCreate(TSHighwayExitCtx* exit, PetscReal postmile,
+				        TSExitType exit_t, TSExitParams* params);
+
+struct _ts_InterchangeCtx{
+
+  PetscInt  exit_road_id; /* id for the road you're leaving */
+  PetscInt  entry_road_id; /* id for the road you're entering */
+  PetscReal postmile; /* posted mile marker location*/
+} PETSC_ATTRIBUTEALIGNED(sizeof(PetscScalar));
+
+typedef struct _ts_InterchangeCtx TSInterchangeCtx;
+
+
+typedef struct {
+  PetscReal   rho; /* traffic density */
+  PetscReal   v; /* traffic speed */
+} HighwayTrafficField;
+
+typedef struct {
+  PetscReal rho_ahead, v_ahead; /* boundary values ahead */
+  PetscReal rho_behind, v_behind; /* boundary values behind */
+} HighwayTrafficBoundary;
+
+struct _ts_HighwayCtx{
+
+  PetscInt           id;/*internal identification for easy indexing*/
+  PetscInt           district=-1; /* district ID (e.g. in CA, comes from Caltrans)*/
+  TSRoadDirection    direction;
+  PetscReal          postmile=-1.0;
+  PetscInt           city_name_length = 0;
+  char               city_name[TS_MAX_CITY_NAME_LEN]=NULL;
+  char               county[TS_COUNTY_NAME_LEN]=NULL;
+  /*if two letters, add a trailing X (e.g. LA -> LAX)*/
+  PetscInt           name_length = 0;
+  char               name[TS_MAX_ROAD_NAME_LEN]=NULL;
+  
+  /* size and location data */
+  PetscReal          length=-1; /* total length of this highway */
+  PetscInt           num_entries=0;
+  TSHighwayEntryCtx* entries=NULL;
+  PetscInt           num_exits=0;
+  TSHighwayExitCtx*  exits=NULL;
+  PetscInt           num_interchanges=0;
+  TSInterchangeCtx*  interchanges=NULL;
+  PetscReal          speed_limit=-1;
+  PetscInt           num_lanes=-1;
+  /* recorded traffic data */
+  PetscReal          back_peak_hour=-1;
+  PetscReal          back_peak_month=-1;
+  PetscReal          back_aadt=-1; /* aadt = annual average daily traffic*/
+  PetscReal          ahead_peak_hour=-1;
+  PetscReal          ahead_peak_month=-1;
+  PetscReal          ahead_aadt=-1;
+
+  /* simulation data */
+  Vec                  rho;
+  HighwayTrafficField  *old_rho;
+  PetscReal            dt;
+  DM                   da;
+  PetscInt             discrete_dimension; /* number of nodes used in DMDA discretization */
+  Mat                  *jac; /* Jacobian */
+} PETSC_ATTRIBUTEALIGNED(sizeof(PetscScalar));
+
+typedef struct _ts_HighwayCtx* TSHighway;
+
+typedef enum {TS_LINEAR, TS_GENERALIZED_LOGISTIC, TS_DECIDE} TSSpeedDensityModel;
+
+extern PetscErrorCode HighwayCreate(MPI_Comm, TSHighway*);
+
+extern PetscErrorCode HighwayDestroy(MPI_Comm, TSHighway*);
+
+extern PetscErrorCode HighwayCreateJacobian(TSHighway, Mat*, Mat*[]);
+
+extern PetscErrorCode HighwayDestroyJacobian(TSHighway);
+
+extern PetscErrorCode HighwayComputeSpeedFromDensity(TSHighway, TSSpeedDensityModel);
+
+extern PetscErrorCode HighwayGetCurrentDensity(TSHighway, PetscInt, PetscReal*, TSSpeedDensityModel);
+
+extern PetscErrorCode HighwayGetCurrentSpeed(TSHighway, PetscInt, PetscReal*, TSSpeedDensityModel);
+
+extern PetscErrorCode HighwaySetIdentification(TSHighway, const PetscInt*, const TSRoadDirection*,
+					       const PetscReal*, const PetscReal*, const PetscInt*,
+					       char*, char*, const PetscInt*, char*);
+
+extern PetscErrorCode HighwaySetTrafficData(TSHighway, const PetscInt*, TSHighwayEntryCtx*,
+					    const PetscInt*, TSHighwayExitCtx*, const PetscInt*,
+					    TSInterchangeCtx*, const PetscReal*, const PetscReal*,
+					    const PetscReal*, const PetscReal*, const PetscReal*,
+					    const PetscReal*, const PetscInt*);
+
+
+extern PetscErrorCode HighwaySetInfo(TSHighway highway, const PetscInt* district,
+				    const TSRoadDirection* direction, const PetscReal* postmile,
+				    const PetscInt* city_name_len, const char* city_name,
+				    const char* county_name, const PetscInt* highway_name_len,
+				    const char* highway_name, const PetscInt* num_entries,
+				    TSHighwayEntryCtx* entries, const PetscInt* num_exits,
+				    TSHighwayExitCtx* exits, const PetscInt* num_interchanges,
+				    TSInterchangeCtx* interchanges, const PetscReal* back_peak_hr,
+				    const PetscReal* back_peak_month, const PetscReal* ahead_peak_hr,
+				     const PetscReal* ahead_peak_month, const PetscReal* ahead_aadt,
+				     const PetscReal* speed_limit, const PetscInt* num_lanes);
+
+extern PetscErrorCode HighwaySetUp(TSHighway);
+
+extern PetscErrorCode HighwaySetCounty(TSHighway, const char*);
+
+extern PetscErrorCode HighwaySetCity(TSHighway, const char*, PetscInt);
+
+extern PetscErrorCode HighwaySetName(TSHighway const char*, PetscInt);
+
+extern PetscErrorCode HighwaySetBackPeakHour(TSHighway, PetscReal);
+
+extern PetscErrorCode HighwaySetAheadPeakHour(TSHighway, PetscReal);
+
+extern PetscErrorCode HighwaySetPeakHour(TSHighway ctx, PetscReal ahead_peak, PetscReal behind_peak);
+
+extern PetscErrorCode HighwaySetBackPeakMonth(TSHighway, PetscReal);
+
+extern PetscErrorCode HighwaySetAheadPeakMonth(TSHighway, PetscReal);
+
+extern PetscErrorCode HighwaySetPeakMonth(TSHighway ctx, PetscReal ahead_peak, PetscReal behind_peak);
+
+extern PetscErrorCode HighwaySetBackAADT(TSHighway, PetscReal);
+
+extern PetscErrorCode HighwaySetAheadAADT(TSHighway, PetscReal);
+
+extern PetscErrorCode HighwaySetAADT(TSHighway ctx, PetscReal ahead_aadt, PetscReal back_aadt);
+
+
+
+#endif
---
+++ TrafficSim/src/highway.c
@@ -0,0 +1,273 @@
+#include "../include/highway.h"
+
+/* subroutines for TSHighway handline */
+
+/*
+  HighwayCreate - Create TSHighway object
+
+  Input parameters:
+  comm - MPI communicator
+
+  Output Parameters:
+  highway - pointer to memory location we want to allocate for the highway
+ */
+
+PetscErrorCode HighwayCreate(MPI_Comm comm, TSHighway* highway)
+{
+  PetscErrorCode ierr;
+  PetscFunctionBegin;
+  ierr = PetscNew(highway);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
+
+/* 
+   HighwaySetIdentification - Set identifying information for a TSHighway
+   object. 
+
+   Input parameters:
+   district - district identification
+   direction - which direction does the highway run? (TS_NS, TS_EW)
+   length - how long is the road (in desired distance units)
+   postmile - posted mile at data collection point
+   highway_name_len - length of highway name in characters
+   highway_name - name of highway
+   county_name - name of county -- MUST BE THREE UPPER-CASE CHARACTERS
+   city_name_len - length of name of city in characters
+   city_name - name of city data is collected in
+   
+   Output parameters:
+   highway - highway whose info we want to set
+
+   Notes:
+   If you don't want to set a particular parameter, pass NULL. If you pass NULL as a name length and do not pass NULL for the corresponding name parameter, 
+   TSim will try to guess the length of the name, but this is UNSAFE; DO NOT DO IT IF YOU HAVE ANY CHOICE
+ */
+
+PetscErrorCode HighwaySetIdentification(TSHighway highway, const PetscInt* district,
+					const TSRoadDirection* direction, const PetscReal* length,
+					const PetscReal* postmile, const PetscInt* highway_name_len,
+					char* highway_name, char* county_name,
+					const PetscInt* city_name_len, char* city_name)
+{
+  PetscFunctionBegin;
+  if PetscLikely(district){
+    highway->district = *district;
+  } else {
+    highway->district = -1;
+  }
+  
+  if PetscLikely(direction){
+    highway->direction = *direction;
+  }
+  else {
+    highway->direction = TS_UNKNOWN_DIRECTION;
+  }
+
+  if(length){
+    if(*length > 0){
+      highway->length = *length;
+    } else {
+      SETERRQ(PETSC_COMM_SELF, PETSC_ERR_ARG_OUTOFRANGE, "Highway length must be positive.\n");
+    }
+  }
+  
+  if PetscLikely(postmile){
+    highway->postmile = *postmile;
+  } else {
+    highway->postmile = -1.0;
+  }
+
+  if(highway_name){
+    if PetscLikely(highway_name_len){
+      if PetscUnlikely(*highway_name_len > TS_MAX_ROAD_NAME_LEN){
+	  SETERRQ(PETSC_COMM_SELF, PETSC_ERR_ARG_SIZ, "highway_name_len (including null-terminator) must be less than or equal to TS_MAX_ROAD_NAME_LEN.\n");
+      } else {
+	highway->name_length = *highway_name_len;
+	highway->name = highway_name;
+      }
+    } else {
+      /* DANGER WARNING */
+      PetcsInt namelen = (PetscInt)(sizeof(highway_name)/sizeof(highway_name[0]));
+      if PetscUnlikely(namelen > TS_MAX_ROAD_NAME_LEN){
+	  SETERRQ(PETSC_COMM_SELF, PETSC_ERR_ARG_SIZ, "highway_name_len (including null-terminator) must be less than or equal to TS_MAX_ROAD_NAME_LEN.\n");
+      } else {
+	highway->name_length = namelen;
+        highway->name = highway_name;
+      }
+    }
+  }
+  else {
+    highway->name_length = -1;
+  }
+
+  if(county_name){
+    if PetscUnlikely(sizeof(county_name)/sizeof(county_name[0]) != TS_COUNTY_NAME_LEN){
+	SETERRQ(PETSC_COMM_SELF, PETSC_ERR_ARG_SIZ, "county_name must have exactly TS_COUNTY_NAME_LEN characters.\n");
+    } else {
+      highway->county = county_name;
+    }
+  }
+
+  if(city_name){
+    if PetscLikely(city_name_len){
+	if PetscUnlikely(*city_name_len > TS_MAX_CITY_NAME_LEN){
+	    SETERRQ(PETSC_COMM_SELF, PETSC_ERR_ARG_SIZ , "city_name_len must be less than or equal to TS_MAX_CITY_NAME_LEN.\n");
+        } else {
+	  highway->city_name_length = *city_name_len;
+	  highway->city_name = city_name;
+	}
+      }
+    else {
+      PetscInt cnamelen = (PetscInt)(sizeof(city_name)/sizeof(city_name[0]));
+      if(PetscUnlikely(cnamelen > TS_MAX_CITY_NAME_LEN)){
+	SETERRQ(PETSC_COMM_SELF, PETSC_ERR_ARG_SIZ , "city_name_len must be less than or equal to TS_MAX_CITY_NAME_LEN.\n");
+      } else {
+	highway->city_name_length = cnamelen;
+	highway->city_name = city_name;
+      }
+    }
+  }
+
+  PetscFunctionReturn(0);
+}
+
+/*
+  HighwaySetTrafficData - Set traffic count data for a TSHighway
+
+  Input parameters:
+  num_entries - how many places can cars enter the highway?
+  entries - array of TSHighwayEntryCtx containing relevant info to the simulation
+  num_exits, exits, num_interchanges, interchanges - same as above, but exits and interchanges instead of entries
+
+  ahead/back input parameters: "ahead" means the count of cars passing by the count station BEFORE they get to the station (and generally intersection, if the station is at intersection), and "back" means the count AFTER the cars have passed the count station. For North-South highways, "ahead" is north of the location, and "back" is south of the location. For East-West highways, "ahead" is east of the station, and "back" is west of the location. (Notation based on Caltrans traffic data reporting notation).
+
+  peak_month - estimate of the avg. daily traffic (ADT) in the largest single month (in terms of ADT) of the year
+  peak_hr - highest one-hour count of the year, excluding some (Caltrans uses 30-50) "outlier" hours with significantly higher-than-normal counts.
+  aadt - annual average daily traffic (AADT) is the average daily traffic count over one year
+  speed_limit - speed limit on this highway
+  num_lanes - number of lanes in each direction
+ */
+PetscErrorCode HighwaySetTrafficData(TSHighway highway, const PetscInt* num_entries,
+				     TSHighwayEntryCtx* entries, const PetscInt* num_exits,
+				     TSHighwayExitCtx* exits, const PetscInt* num_interchanges,
+				     TSInterchangeCtx* interchanges, const PerscReal* back_peak_hr,
+				     const PetscReal* back_peak_month, const PetscReal* back_aadt,
+				     const PetscReal* ahead_peak_hr, const PetscReal* ahead_peak_month,
+				     const PetscReal* ahead_aadt, const PetscReal* speed_limit,
+				     const PetscInt* num_lanes)
+{
+  PetscFunctionBegin;
+  if(entries){
+    if(num_entries){
+      if(*num_entries >= 0){
+	highway->num_entries = *num_entries;
+	highway->entries = entries;
+      } else {
+	SETERRQ(PETSC_COMM_SELF, PETSC_ERR_ARG_OUTOFRANGE, "Number of entries must be positive or zero.\n");
+      }
+    }
+  }/*entries*/
+
+  if(exits){
+    if(num_exits){
+      if(*num_exits >= 0){
+	highway->num_exits = *num_exits;
+	highway->exits = exits;
+      } else {
+	SETERRQ(PETSC_COMM_SELF, PETSC_ERR_ARG_OUTOFRANGE, "Number of exits must be positive or zero.\n");
+      }
+    } 
+  }/*exits*/
+
+
+  if(interchanges){
+    if(num_interchanges){
+      if(*num_interchanges >= 0){
+	highway->num_interchanges = *num_interchanges;
+	highway->interchanges = interchanges;
+      } else {
+	SETERRQ(PETSC_COMM_SELF, PETSC_ERR_ARG_OUTOFRANGE, "Number of entries must be positive or zero.\n");
+      }
+    }
+  }/*interchanges*/
+
+  if(back_peak_hr){
+    if(*back_peak_hr >=0){
+      highway->back_peak_hour = *back_peak_hour;
+    } else {
+      SETERRQ(PETSC_COMM_SELF, PETSC_ERR_ARG_OUTOFRANGE, "*back_peak_hr must be positive or zero.\n");
+    }
+  }
+
+  if(ahead_peak_month){
+    if(*ahead_peak_month >=0){
+      highway->ahead_peak_month = *ahead_peak_month;
+    } else {
+      SETERRQ(PETSC_COMM_SELF, PETSC_ERR_ARG_OUTOFRANGE, "*ahead_peak_month must be positive or zero.\n");
+    }
+  }
+
+    if(back_peak_month){
+    if(*back_peak_month >=0){
+      highway->back_peak_month = *back_peak_month;
+    } else {
+      SETERRQ(PETSC_COMM_SELF, PETSC_ERR_ARG_OUTOFRANGE, "*back_peak_month must be positive or zero.\n");
+    }
+  }
+
+  if(ahead_peak_hr){
+    if(*ahead_peak_hr >=0){
+      highway->ahead_peak_hour = *ahead_peak_hour;
+    } else {
+      SETERRQ(PETSC_COMM_SELF, PETSC_ERR_ARG_OUTOFRANGE, "*ahead_peak_hr must be positive or zero.\n");
+    }
+  }
+
+
+  if(back_aadt){
+    if(*back_aadt >=0){
+      highway->back_aadt = *back_aadt;
+    } else {
+      SETERRQ(PETSC_COMM_SELF, PETSC_ERR_ARG_OUTOFRANGE, "*back_aadt must be positive or zero.\n");
+    }
+  }
+
+  if(ahead_aadt){
+    if(*ahead_aadt >=0){
+      highway->ahead_aadt = *ahead_aadt;
+    } else {
+      SETERRQ(PETSC_COMM_SELF, PETSC_ERR_ARG_OUTOFRANGE, "*ahead_aadt must be positive or zero.\n");
+    }
+  }
+
+  if(speed_limit){
+    if(*speed_limit >=0){
+      highway->speed_limit = *speed_limit;
+    } else {
+      SETERRQ(PETSC_COMM_SELF, PETSC_ERR_ARG_OUTOFRANGE, "*speed_limit must be positive or zero.\n");
+    }
+  }
+
+  if(num_lanes){
+    if(*num_lanes >=0){
+      highway->num_lanes = *num_lanes;
+    } else {
+      SETERRQ(PETSC_COMM_SELF, PETSC_ERR_ARG_OUTOFRANGE, "*num_lanes must be positive or zero.\n");
+    }
+  }
+  
+  PetscFunctionReturn(0);
+}
+        
+	  
+
+      
+
+  
+
+  
+
+  
+					
+					
